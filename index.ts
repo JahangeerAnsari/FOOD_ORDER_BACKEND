3e83092caf98a9ef9e997f58e72afee2e5d7682b
Jahangeer Ansari <jahangeeransari194@gmail.com>
2024-10-31 01:39:13 -0700

    node install

--- index.ts
+++ index.ts
@@ -2,8 +2,10 @@ import express from "express";
 import {adminRoute,vandorRoute} from './routes'
 const app = express();
 
-app.use('admin',adminRoute)
-app.listen(8000,() =>{
- console.log("App listening on port :8000 ");
+app.use('/admin',adminRoute)
+
+
+app.listen(8100,() =>{
+ console.log("App listening on port :81000 ");
 
 })
